bd120330a49ded00ba055a24825c5d07ae0e28f8
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-02 07:37:36 +0200

    add tweets

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const routes = [{
+    path: '/',
+    component: {
+      template: `<div><div v-for="r in rows" class="card text-white bg-dark mb-3"><div class="card-header"><img :src="r.img" class="w-5 h-5 rounded-full">{{r.username}}</div><div class="card-body" v-html="r.text"></div></div></div>`,
+      data() {
+        return {
+          rows: [{
+            img:"/img/Avatar.JPG",
+            username: "Hobby",
+            text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
+          }, {
+            img:"/img/Avatar.jpg",
+            username: "Hobby",
+            text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
+          }]
+        }
+      }
+    }
+  },
+  {
+    path: '/bar',
+    component: {
+      template: '<div>bar</div>'
+    }
+  },
+  {
+    path: '*',
+    component: {
+      template: '<div>404 Not Found</div>'
+    }
+  }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+const app = new Vue({
+  router: router,
+  data: {
+    rows: [{
+      username: "Hobby",
+      text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
+    }, {
+      username: "Hobby",
+      text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
+    }]
+  }
+}).$mount('#app')
