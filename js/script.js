96cad23a7d14332c36ec91bdce7e586a1e2d5f8c
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-02 22:07:40 +0200

    use accordion on acknowledgment page

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@ const routes = [{
         return {
           rows: [{
               img: "/img/avatar.jpg",
-              round:true,
+              round: true,
               username: "Hobby",
               text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
             }, {
               img: "/img/avatar.jpg",
-              round:true,
+              round: true,
               username: "Hobby",
               text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
             },
             {
               img: "/img/quotable.svg",
-              round:false,
+              round: false,
               username: "Quotable",
               text: `<figure>
   <blockquote class="blockquote">
@@ -39,10 +39,57 @@ const routes = [{
     component: {
       template: '<div>{{$route.params.name}}</div>'
     }
-  },{
+  }, {
     path: '/acknowledgements',
     component: {
-      template: '<article><h1>Acknowledgements</h1><h5>These awesome open source projects made this site possible:</h5><ul><li>Bootstrap</li></ul></article>'
+
+      data() {
+        return {
+          licenses: [{
+            name: "Bootstrap",
+            text: `The MIT License (MIT)
+
+  Copyright (c) 2011-2021 Twitter, Inc.
+  Copyright (c) 2011-2021 The Bootstrap Authors
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.`
+},{name:"Vue", text:"A license"}]
+        }
+      },
+      template: `<article><h1>Acknowledgements</h1><h5>These awesome open source projects made this site possible:</h5>
+      <div class="accordion" id="accordion">
+        <div class="accordion-item"  v-for="item,i in licenses">
+          <h2 class="accordion-header" :id="'heading'+i">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse'+i" aria-expanded="false" aria-controls="collapseOne">
+              {{item.name}}
+            </button>
+          </h2>
+          <div :id="'collapse'+i" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion">
+            <div class="accordion-body">
+              {{item.text}}
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </article>`
+
     }
   },
   {
@@ -60,7 +107,7 @@ const router = new VueRouter({
 const app = new Vue({
   router: router,
   data: {
-    loading:false,
+    loading: false,
     rows: [{
       username: "Hobby",
       text: "I like drawing! Check out my newest drawings on behance: <a href=\"https://mpm.is-a.dev/be\">https://mpm.is-a.dev/be</a>"
